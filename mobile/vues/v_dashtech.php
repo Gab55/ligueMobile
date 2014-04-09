d55a2d77f01c043f5ff5131fb33575509ced3d7f
elouyot3 <edouard88100@aim.com>
2014-04-09 17:25:01 +0200

    modif fonction

--- mobile/vues/v_dashtech.php
+++ mobile/vues/v_dashtech.php
@@ -28,38 +28,22 @@
 
                     <table data-role="table" id="table-column-toggle" data-mode="columntoggle" class="ui-responsive table-stroke">
                         <thead>
-                        <tr>
-                            <th>Client</th>
-                            <th>Statut</th>
-                            <th data-priority="3">Date</th>
-                            <th data-priority="4">Resume</th>
-                            <th></th>
-                        </tr>
-                        </thead>
+                        <tr><th>Client</th><th>Crée le</th><th>Statut</th><th>Description</th><th>PRIORITÉ</th><th>Modifier</th></tr>
                         <?php
                         foreach($bugs_en_cours as $bug){
+                            $engineer = "Non déterminé";
                             if ($bug->getEngineer() != null){
                                 $engineer = $bug->getEngineer()->getName();
-                            }else{
-                                $engineer = "non affecté";
                             }
-
                             echo "<tr>";
-                            //echo "<td>".$engineer."</td>";
-                            echo "<td>".$bug->getReporter()->getName()."</td>";
-                            //echo "<td>".$bug->getDescription()."</td>";
-                            if($bug->getStatus()=="IN PROGRESS"){
-                                echo "<td>En cours</td>";
-                            }
-                            else {
-                                echo "<td>Ouvert</td>";
-                            }
+                            echo "<td>".$engineer."</td>";
                             echo "<td>".$bug->getCreated()->format('d.m.Y')."</td>";
-                            echo "<td>".$bug->getResume()."</td>";
+                            echo "<td>".$bug->getStatus()."</td>";
+                            echo "<td>".$bug->getDescription()."</td>";
+                            echo "<td>".$bug->getDelai()."</td>";
                             echo "<td><a href='index.php?uc=dash&action=config&bug=".$bug->getId()."'><img src='util/img/arrow.png'></a></td>";
                             echo "</tr>";
                         }
-
                         ?>
                     </table><br>
                 </div>
